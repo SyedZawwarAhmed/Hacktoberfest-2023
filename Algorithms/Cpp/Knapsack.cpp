cb4bc9c1236d8854709cb9ca773f620d90f4c804
Raksha703 <85754170+Raksha703@users.noreply.github.com>
2023-10-22 22:33:46 +0530

    Create Knapsack.cpp

---
+++ Algorithms/Cpp/Knapsack.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int knapsack(vector<int>& weights, vector<int>& values, int capacity) {
+    int n = weights.size();
+    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 1; w <= capacity; w++) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][capacity];
+}
+
+int main() {
+    vector<int> weights = {1, 3, 4, 5};
+    vector<int> values = {1, 4, 5, 7};
+    int capacity = 7;
+    cout << "Maximum value that can be obtained: " << knapsack(weights, values, capacity) << endl;
+    return 0;
+}
